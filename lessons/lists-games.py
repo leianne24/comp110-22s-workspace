a6c46618c7153a0af03536dc84c104e092035548
leianne24 <leianne@live.unc.edu>
2022-02-09 22:48:38 -0500

    Uodates

---
+++ lessons/lists-games.py
@@ -0,0 +1,8 @@
+"""A list game: exapmples of using lists in a simple game thing?"""
+
+
+from random import randint
+
+
+randint
+rolls: list[int]
--- sandbox/bits_and_pieces.py
+++ sandbox/bits_and_pieces.py
@@ -1,6 +1,46 @@
 """Just noodlin around."""
 
+      
+        
+# if i > 6:
+        #     print (f"X/6 - Sorry, try again tomorrow!")
+    
+    
 
+
+        # else:
+        #     j: int = 0
+        #     j: bool = False
+        #     while j < len(guess):
+        #         if guess[j] == secret[index]: #wouldn't hte while statement just keep repeating without adding onto the emoji string?
+        #             bool_found = True
+        #         j = j + 1
+        #     if bool_found == True: #why shouldn't these be indented?
+        #         emoji = emoji + YELLOW_BOX
+        #     else:
+        #         emoji = emoji + WHITE_BOX
+    
+
+
+
+    # index += 1
+    # return emoji
+
+
+
+
+# while index <= len(guess):    
+    #     if  contains_char(guess, guess[index]) == False:
+    #         emoji += WHITE_BOX
+        # while contains_char(guess, guess[index]) == True:             # What did I forget here?
+        #     emoji += GREEN_BOX
+        # else:
+        #     emoji += YELLOW_BOX
+
+#print(emojified("elloh", "hello"))
+#from exercises.ex03_wordle import emojified
+#from exercises.ex03_wordle import input_guess  
+#print(input_guess(6))
 
 # Code Corner
 # return
