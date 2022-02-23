7b6565d4aae36dec92fb6f98ca1f5365ae4dd9a8
leianne24 <leianne@live.unc.edu>
2022-02-23 13:25:17 -0500

    test things

---
+++ exercises/utils_test.py
@@ -0,0 +1,12 @@
+"""The testing of functions."""
+__author__ = "730308277"
+
+# python -m pytest exercises/ex05
+
+# one edge case and two use cases
+# python -m pytest exercises/ex05 
+
+# from utils import only_evens
+# from utils import sub
+# from utils import concat
+# from utils import only_evens, sub, concat
