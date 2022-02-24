3c98b50d28de54e2faee26527294585b50f6478e
leianne24 <leianne@live.unc.edu>
2022-02-23 22:54:50 -0500

    Changes

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -19,15 +19,28 @@ def sub(a_list: list[int], s: int, e: int) -> list[int]:
     if s < 0:
         s = 0
     i: int = s
-    e = e - 1
-    while e > i:
-        
-        if e > len(a_list):
-            e = len(a_list)
-    b_list.append(a_list[i])  
-    i += 1
+    if e >= len(a_list):
+        e = len(a_list) - 1
+    else:
+        e = e - 1
+    while e >= i:
+        part: int = (a_list[i])
+        b_list.append(part)  
+        i += 1
     return b_list
 
 
+def concat(l_one: list[int], l_two: list[int]) -> list[int]:
+    """Will make list two concatenate onto the end of list two."""
+    m_list: list[int] = []
+    for item in l_one: 
+        m_list.append(item)
+    for item_t in l_two:
+        m_list.append(item_t)
+    return m_list
+
+
 # from exercises.ex05.utils import only_evens
-# from exercises.ex05.utils import sub  
+# from exercises.ex05.utils import sub
+# from exercises.ex05.utils import concat
+# concat([10, 20], [30, 40])
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,12 +1,56 @@
 """The testing of functions."""
 __author__ = "730308277"
 
-# python -m pytest exercises/ex05
+from utils import only_evens, sub, concat
 
-# one edge case and two use cases
-# python -m pytest exercises/ex05 
 
-# from utils import only_evens
-# from utils import sub
-# from utils import concat
-# from utils import only_evens, sub, concat
+def test_only_evens_two() -> None:
+    """To test if our only_evens function will return the even values."""
+    assert only_evens([8, 6, 9, 4, 7, 10]) == [8, 6, 4, 10]
+
+
+def test_only_evens() -> None:
+    """To test if our only_evens function will return the even values."""
+    assert only_evens([1, 2, 4]) == [2, 4]
+
+
+def test_only_evens_empty() -> None:
+    """To test if our only_evens function will return an empty string."""
+    assert only_evens([]) == []
+
+
+def test_sub_two() -> None:
+    """To test if our sub function will return the indexed values."""
+    assert sub([2, 4, 6, 8, 10], 3, 4) == [8]
+
+
+def test_sub() -> None:
+    """To test if our sub function will return the proper values."""
+    assert sub([1, 2, 4], 0, 4) == [1, 2, 4]
+
+
+def test_sub_empty() -> None:
+    """To test our only_evens function will an empty string."""
+    assert sub([], 0, 1) == []
+
+
+def test_concat_two() -> None:
+    """To test our only_evens function will return the proper values."""
+    assert concat([3, 6, 9], [5, 6, 9, 5]) == [3, 6, 9, 5, 6, 9, 5]
+
+
+def test_only_concat() -> None:
+    """To test our only_evens function will return the proper values."""
+    assert concat([8, 6, 4, 2], [1, 3, 5, 7]) == [8, 6, 4, 2, 1, 3, 5, 7]
+
+
+def test_concat_empty() -> None:
+    """To test our only_evens function will an empty string."""
+    assert concat([], []) == []
+
+# # python -m pytest exercises/ex05
+# # one edge case and two use cases
+# # python -m pytest exercises/ex05 
+# # from utils import only_evens
+# # from utils import sub
+# # from utils import concat
